5eab9d60dde0c0f59c284e1a639b607678e9d521
degoldschmidt <goldschmidt.de@gmail.com>
2016-12-16 13:37:56 +0000

    fps test

--- tests/grab.py
+++ tests/grab.py
@@ -1,7 +1,9 @@
 import numpy as np
 import cv2
 import os
+from imutils.video import FPS
 os.system("sudo modprobe bcm2835-v4l2")
+fps = FPS().start()
 
 cap = cv2.VideoCapture(0)
 if cap.isOpened() == False:
@@ -14,12 +16,16 @@ else:
         if ret==False:
             print('ERROR: Unable to grab from the camera')
             break;
- 
+
         cv2.imshow('Live',frame)
         if cv2.waitKey(1) >= 0:
            break
     print('Closing the camera')
- 
+
 cap.release()
 cv2.destroyAllWindows()
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 print('Done\n')
